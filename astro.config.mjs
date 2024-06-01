7c53921ecc1b0d832f595b599fabb816a825d77c
Faareoh <m.raoux34@gmail.com>
2024-06-01 19:00:52 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://e-bou.fr',
+	integrations: [
+		starlight({
+			title: 'E-bou',
+			locales: {
+				root: {
+					label: 'Français',
+					lang: 'fr',
+				},
+			},
+			logo: {
+				dark: './src/assets/E-bou.V2.png',
+				light: './src/assets/E-bou.V2.png',
+			},
+			social: {
+				twitter: 'https://twitter.com/EbouBot',
+				discord: 'https://discord.gg/rmSsDC84AS',
+				github: 'https://github.com/E-bou',
+			},
+			sidebar: [
+				{
+					label: 'Changelog',
+					autogenerate: { directory: 'changelog' },
+				}
+			],
+			lastUpdated: true,
+		}),
+	],
+});
