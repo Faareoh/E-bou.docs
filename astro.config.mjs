e2fec04d9939b19fad9103799942cb0de43065ed
Faareoh <m.raoux34@gmail.com>
2025-06-15 22:48:39 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,15 +18,15 @@ export default defineConfig({
 				dark: './src/assets/logo.png',
 				light: './src/assets/logo.png',
 			},
-			social: {
-				twitter: 'https://twitter.com/EbouBot',
-				discord: 'https://discord.gg/rmSsDC84AS',
-				github: 'https://github.com/Faareoh/E-bou.docs',
-			},
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/Faareoh/E-bou.docs' },
+				{ icon: 'twitter', label: 'Twitter', href: 'https://twitter.com/EbouBot' },
+				{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/rmSsDC84AS' }
+			],
 			sidebar: [
 				{
-					label: 'Changelog',
-					autogenerate: { directory: 'changelog' },
+					label: 'Informations',
+					autogenerate: { directory: 'app' },
 				}
 			],
 		}),
